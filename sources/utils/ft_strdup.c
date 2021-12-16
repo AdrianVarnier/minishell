3f1936b9d4697be4d32aa202b301c336996e7802
Adrian Varnier <arcadia@iMac-de-Adrian.home>
2021-12-16 12:37:58 +0100

    error correction on ft_strdup

--- sources/utils/ft_strdup.c
+++ sources/utils/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: avarnier <avarnier@stduent.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 13:06:23 by avarnier          #+#    #+#             */
-/*   Updated: 2021/12/15 20:20:25 by avarnier         ###   ########.fr       */
+/*   Updated: 2021/12/16 12:37:35 by avarnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_strdup(char *s)
 		return (NULL);
 	i = 0;
 	len = ft_strlen(s) + 1;
-	cpy = malloc(sizeof(char *) * len);
+	cpy = malloc(sizeof(char) * len);
 	if (cpy == NULL)
 		return (NULL);
 	ft_memcpy(cpy, s, len);
