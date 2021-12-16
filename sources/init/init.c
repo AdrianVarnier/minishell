ad3a9a5b0301b7a16cf31a404e0d63fc7ac2c07e
Arthur LI <ali@e2r11p10.clusters.42paris.fr>
2021-12-16 15:58:23 +0100

    init

--- sources/init/init.c
+++ sources/init/init.c
@@ -1,30 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parse_env.c                                        :+:      :+:    :+:   */
+/*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ali <ali@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/16 15:27:15 by ali               #+#    #+#             */
-/*   Updated: 2021/12/16 15:45:12 by ali              ###   ########.fr       */
+/*   Created: 2021/12/16 15:37:05 by ali               #+#    #+#             */
+/*   Updated: 2021/12/16 15:56:24 by ali              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_env	*ft_parse_env(char **envp)
+int	ft_readline(t_env *env)
+{
+	t_cmd	*cmd;
+	t_cmd	*to_free;
+	char	*ret;
+
+	ret = readline("minishell>$");
+	while (ret)
+	{
+		add_history(ret);
+		cmd = ft_parse_line(ret, &env);
+		to_free = cmd;
+		exec_cmd(cmd, env);
+		free(ret);
+		free_cmd(to_free);
+		ret = readline("minishell>$");
+	}
+}
+
+int	main(int ac, char **av, char **envp)
 {
 	t_env	*env;
-	int		i;
 
 	env = NULL;
-	i = 0;
-	while (envp[i])
-	{
-		add_to_env(ft_get_key(envp[i]), ft_get_value(envp[i], &env));
-		if (env == NULL)
-			return (NULL);
-		i++;
-	}
-	return (env);
+	env = ft_parse_env(envp);
+	ft_readline(env);
+	free_env(env);
 }
