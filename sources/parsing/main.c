f8434a5aff291058ec41ca9d232a6e5c004ca124
Arthur LI <ali@e2r4p22.clusters.42paris.fr>
2021-12-17 12:23:42 +0100

    backup

--- sources/parsing/main.c
+++ sources/parsing/main.c
@@ -6,47 +6,21 @@
 /*   By: ali <ali@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 15:51:33 by ali               #+#    #+#             */
-/*   Updated: 2021/12/15 18:22:14 by ali              ###   ########.fr       */
+/*   Updated: 2021/12/17 12:15:36 by ali              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-
-int	main(void)
+int	main(int ac, char **av, char **envp)
 {
-	char		*ret;
-	t_cmd		*cmds;
-	int			i;
-	t_env		*env = NULL;
-	t_cmd		*to_free;
+	t_env	*env;
 
-	add_to_env("TEST", "blubiblub", &env);
-	add_to_env("TEST2", "crouchcrouch", &env);
-	add_to_env("TEST3", "nenenenene nenenen", &env);
-	ret = readline("prompt>");
-	while (ret)
-	{
-		add_history(ret);
-		cmds = ft_parse_line(ret, &env);
-		to_free = cmds;
-		while (cmds)
-		{
-			printf("\nCMD\n");
-			i = 0;
-			while (cmds->args[i])
-			{
-				printf("arg:%s\n", cmds->args[i]);
-				i++;
-			}
-			printf("infile = %s\noutfile = %s\n", cmds->infile, cmds->outfile);
-			cmds = cmds->next;
-		}
-		free(ret);
-		free_cmd(to_free);
-		ret = readline("prompt>");
-	}
+	(void) ac;
+	(void) av;
+	env = NULL;
+	env = ft_parse_env(envp);
+	ft_readline(env);
 	free_env(env);
 }
-
 
