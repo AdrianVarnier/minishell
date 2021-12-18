392937dfff37d2893f190d828fbb70881ba0f1fb
Adrian Varnier <arcadia@pc-49.home>
2021-12-18 01:38:22 +0100

    waitpid change exec_all_cmd

--- sources/exec/exec_all_cmd.c
+++ sources/exec/exec_all_cmd.c
@@ -6,17 +6,35 @@
 /*   By: avarnier <avarnier@stduent.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 22:16:04 by avarnier          #+#    #+#             */
-/*   Updated: 2021/12/18 00:52:12 by avarnier         ###   ########.fr       */
+/*   Updated: 2021/12/18 01:37:17 by avarnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	cmd_size(t_cmd *cmd)
+{
+	int	i;
+
+	i = 0;
+	if (cmd == NULL)
+		return (0);
+	while (cmd != NULL)
+	{
+		if (!(is_builtin(cmd->args[0]) == 1 && cmd->output_type != PIPE))
+			i++;
+		cmd = cmd->next;
+	}
+	return (i);
+}
+
 void	exec_all_cmd(t_cmd *cmd, t_env *env)
 {
+	int		i;
 	int		status;
-	pid_t	pid;
+	pid_t	pid[cmd_size(cmd)];
 
+	i = 0;
 	if (check_file(cmd) == 0)
 		return ;
 	while (cmd != NULL)
@@ -27,16 +45,17 @@ void	exec_all_cmd(t_cmd *cmd, t_env *env)
 			exec_builtin(cmd, env);
 		else
 		{
-			pid = fork();
-			if (pid == 0)
+			pid[i] = fork();
+			if (pid[i] == 0)
 				exec_cmd(cmd, env);
+			i++;
 		}
 		if (cmd->input_type == PIPE)
 		{
 			close(cmd->input);
 			close(cmd->prev->output);
 		}
+		waitpid(pid[i], &status, 0);
 		cmd = cmd->next;
 	}
-	waitpid(pid, &status, 0);
 }
