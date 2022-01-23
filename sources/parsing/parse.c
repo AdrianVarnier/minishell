3f040cc79f07d210ac6a5d6365aeea9eeb36475e
Arthur Li <li.arthur101@gmail.com>
2022-01-24 00:46:47 +0100

    correction leak sur le parsing

--- sources/parsing/parse.c
+++ sources/parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: ali <ali@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 17:41:58 by ali               #+#    #+#             */
-/*   Updated: 2022/01/23 15:31:35 by ali              ###   ########.fr       */
+/*   Updated: 2022/01/24 00:46:25 by ali              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ t_cmd	*ft_parse_line(char *line, t_env **env)
 {
 	char	**strs;
 	t_cmd	*cmd;
+	char	*exit;
 
 	cmd = NULL;
 	strs = ft_splitline(line);
@@ -24,10 +25,11 @@ t_cmd	*ft_parse_line(char *line, t_env **env)
 		free_char2(strs);
 		return (NULL);
 	}
+	exit = ft_itoa(g_exit);
 	if (!get_env("?", *env))
-		add_to_env("?", ft_itoa(g_exit), env);
+		add_to_env("?", exit, env);
 	else
-		set_env("?", ft_itoa(g_exit), *env);
+		set_env("?", exit, *env);
 	ft_variables(strs, env);
 	cmd = ft_stock_cmd(strs);
 	free_char2(strs);
