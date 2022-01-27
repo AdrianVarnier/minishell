83f8b518bc2b0761587122f529ccf0caa7663942
Arthur Li <li.arthur101@gmail.com>
2022-01-27 18:03:05 +0100

    gestion des variables dans les noms de heredoc, et gestion du cas sans commande

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: avarnier <avarnier@stduent.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 12:11:56 by avarnier          #+#    #+#             */
-/*   Updated: 2022/01/27 15:12:48 by ali              ###   ########.fr       */
+/*   Updated: 2022/01/27 16:06:56 by ali              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -143,6 +143,7 @@ char	*ft_no_spaces(char *str);
 int	ft_nospaces_size(char *str);
 int	ft_has_no_quotes(char *str);
 void	ft_quotes(char **strs);
+char	*ft_variables_heredoc(char *str);
 
 // signals
 
