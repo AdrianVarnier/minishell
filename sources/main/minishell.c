1483d36de6554ad07e10cbd0d7f9017de484186d
Arthur Li <li.arthur101@gmail.com>
2022-01-30 00:59:31 +0100

    update SHLVL

--- sources/main/minishell.c
+++ sources/main/minishell.c
@@ -6,13 +6,22 @@
 /*   By: avarnier <avarnier@stduent.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 16:38:12 by avarnier          #+#    #+#             */
-/*   Updated: 2022/01/29 22:45:15 by avarnier         ###   ########.fr       */
+/*   Updated: 2022/01/30 00:58:45 by ali              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 int	g_exit;
+
+void	ft_up_shlvl(t_env * env)
+{
+	int	lvl;
+
+	lvl = ft_atoi(get_env("SHLVL", env));
+	lvl++;
+	set_env("SHLVL", ft_itoa(lvl), env);
+}
 
 int	main(int ac, char **av, char **envp)
 {
@@ -24,6 +33,7 @@ int	main(int ac, char **av, char **envp)
 	g_exit = 0;
 	env = ft_parse_env(envp);
 	add_to_env("?", "0", &env);
+	ft_up_shlvl(env);
 	ft_signal(1);
 	ft_readline(&env);
 	free_env(env);
